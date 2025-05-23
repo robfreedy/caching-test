44ab5eb00c72fc0715e68e2fae4aac584dd9f198
Rob Freedy <robert@prefect.io>
2025-05-23 10:31:16 -0400

    Fix randint

--- flow.py
+++ flow.py
@@ -1,10 +1,10 @@
 from prefect import flow, task, get_run_logger
 from prefect.cache_policies import TASK_SOURCE, RUN_ID
-import random
+from random import randrange
 
 @task(cache_policy=TASK_SOURCE+RUN_ID)
 def random_int():
-    return random.randomint()
+    return randrange(10)
 
 @flow
 def test_caching_policies():
